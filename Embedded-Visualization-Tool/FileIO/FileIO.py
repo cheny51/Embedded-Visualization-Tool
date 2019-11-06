0682cd636f751cb36dac33306f85328b11a2b7cf
Aiden <cheny51@rpi.edu>
2019-11-06 00:10:01 -0500

    fileIOTab update
    contain import, export option for file and config.
    still need to do method to call to operate the said purpose.

--- Embedded-Visualization-Tool/FileIO/FileIO.py
+++ Embedded-Visualization-Tool/FileIO/FileIO.py
@@ -9,15 +9,12 @@ includes the following:
 Creator: Aiden Chen  
 '''
 # standard libraries
-import sys
 
 # third party libraries
-from PyQt5.QtWidgets import QWidget, QTableView, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QTableView, QVBoxLayout, QCheckBox
 from PyQt5.QtWidgets import QGridLayout, QLabel, QPushButton, QLineEdit
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
-# from PyQt5.QtGui import QIcon
-
 
 class FiloIOTab(QWidget):
     '''This class houses all the set widgets used in the
@@ -36,14 +33,14 @@ class FiloIOTab(QWidget):
         # file io tab label
         self.fileio_tab_title_label = QLabel()
         self.fileio_tab_title_label.setFont(QFont('Arial', 10))
-        self.fileio_tab_title_label.setText('<b>File Import Tab:</b>')
+        self.fileio_tab_title_label.setText('<b>File IO Tab</b>')
         self.fileio_layout.addWidget(self.fileio_tab_title_label)
-        
+
         # define the inner structure below the header
-        self.fileio_tab_layout_grid = QGridLayout()
+        self.fileio_tab_import_layout_grid = QGridLayout()
 
         # File select button
-        self.fileio_tab_select_file_pushbutton = QPushButton('Select File Here')
+        self.fileio_tab_select_file_pushbutton = QPushButton('Select Folder Here')
 
         # File selected directory display box, for display only
         self.fileio_tab_select_file_lineedit = QLineEdit('')
@@ -54,19 +51,73 @@ class FiloIOTab(QWidget):
         #       empty file imported
 
         # Import selected file push button
-        self.fileio_tab_import_selected_pushbutton = QPushButton('Import File Here')
+        self.fileio_tab_import_selected_pushbutton = QPushButton('Import File Within Folder Here')
         self.fileio_tab_import_selected_pushbutton.setFixedWidth(250)
         # TODO: add in method that convert csv to a valid dataframe
+
+        # Import Status
+        # TODO: Add a progress bar, and show a dialogue upon finishes
+        self.fileio_tab_import_selected_label = QLabel()
+        self.fileio_tab_import_selected_label.setFont(QFont('Arial', 10))
+        self.fileio_tab_import_selected_label.setText('Import Status: unimported')
+
+        # Import Configuration Parameters
+        self.fileio_tab_import_configuration_layout = QVBoxLayout()
+
+        # Import Configuration File PushButton
+        self.fileio_tab_import_configuration_pushbutton = QPushButton("Select Config File")
+        # Import Configuration Confirm PushButton
+        self.fileio_tab_import_config_confirm_pushbutton = QPushButton("Import Config File")
+        # Import Configuration State
+        self.fileio_tab_import_config_label = QLabel('No Config Imported')
+        self.fileio_tab_import_config_label.setAlignment(Qt.AlignCenter)
+
+        self.fileio_tab_import_configuration_layout.addWidget(self.fileio_tab_import_configuration_pushbutton)
+        self.fileio_tab_import_configuration_layout.addWidget(self.fileio_tab_import_config_confirm_pushbutton)
+        self.fileio_tab_import_configuration_layout.addWidget(self.fileio_tab_import_config_label)
 
         # Import selected file tableview
         self.fileio_tab_import_selected_table = QTableView()
         self.fileio_tab_import_selected_table.setFixedHeight(200)
 
-        self.fileio_tab_layout_grid.addWidget(self.fileio_tab_select_file_pushbutton, 0, 0)
-        self.fileio_tab_layout_grid.addWidget(self.fileio_tab_select_file_lineedit, 0, 1)
-        self.fileio_tab_layout_grid.addWidget(self.fileio_tab_import_selected_pushbutton, 1, 0)
-        self.fileio_tab_layout_grid.addWidget(self.fileio_tab_import_selected_table, 1, 1)
-        self.fileio_layout.addLayout(self.fileio_tab_layout_grid)
+        # Add all import related widgets
+        self.fileio_tab_import_layout_grid.addWidget(self.fileio_tab_select_file_pushbutton, 0, 0)
+        self.fileio_tab_import_layout_grid.addWidget(self.fileio_tab_select_file_lineedit, 0, 1)
+        self.fileio_tab_import_layout_grid.addWidget(self.fileio_tab_import_selected_pushbutton, 1, 0)
+        self.fileio_tab_import_layout_grid.addWidget(self.fileio_tab_import_selected_label, 1, 1)
+        self.fileio_tab_import_layout_grid.addLayout(self.fileio_tab_import_configuration_layout, 2, 0, Qt.AlignVCenter)
+        self.fileio_tab_import_layout_grid.addWidget(self.fileio_tab_import_selected_table, 2, 1)
+        self.fileio_layout.addLayout(self.fileio_tab_import_layout_grid)
         self.fileio_layout.addStretch(20)
+
+        # Output Directory Parameters
+        self.fileio_tab_export_title_label = QLabel()
+        self.fileio_tab_export_title_label.setFont(QFont('Arial', 10))
+        self.fileio_tab_export_title_label.setText('<b>Output Directory Parameters: </b>')
+        self.fileio_layout.addWidget(self.fileio_tab_export_title_label)
+
+        # Add in the grid for output
+        self.fileio_tab_output_layout_grid = QGridLayout()
+
+        # checkbox to enable output graphing plots or not
+        self.fileio_tab_export_checkbox = QCheckBox("Save output offline")
+
+        # TODO: add in method to respond to checkbox state
+        #       checked: output, enable line editor, unchecked: don't output
+
+        # Select directory to output to
+        self.fileio_tab_output_select_directory_pushbutton = QPushButton("Save Plot To Folder...")
+        self.fileio_tab_output_select_directory_pushbutton.setFixedWidth(250)
+        self.fileio_tab_output_select_directory_lineedit = QLineEdit('')
+        self.fileio_tab_output_select_directory_lineedit.setReadOnly(True)
+        
+        # TODO: add in connect method to add the directory selected to the lineedit
+
+
+        # Add in all export related widgets
+        self.fileio_tab_output_layout_grid.addWidget(self.fileio_tab_export_checkbox, 0, 0)
+        self.fileio_tab_output_layout_grid.addWidget(self.fileio_tab_output_select_directory_pushbutton, 1, 0)
+        self.fileio_tab_output_layout_grid.addWidget(self.fileio_tab_output_select_directory_lineedit, 1, 1)
+        self.fileio_layout.addLayout(self.fileio_tab_output_layout_grid)
 
         self.setLayout(self.fileio_layout)
